5e95646ead41d862ba33d56bb054cbb0ecb6b740
SACCO <jennyrae818@gmail.com>
2022-04-23 12:54:02 -0700

    added env and gitignore and started js code

--- models/index.js
+++ models/index.js
@@ -5,12 +5,15 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey: 'category_id'
+});
 
-// Categories have many Products
+// Categories have many Products / hasMany / 
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags (through ProductTag) / belongsToMany
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belongToMany Products (through ProductTag) / belongsToMany
 
 module.exports = {
   Product,
--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+//middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// turns on routes 
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
+sequelize.sync().then(() => {
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
+  });
 });
+
